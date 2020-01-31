1514f73748d5f3a54f49a524acffdc9e89bfe797
Matthew Widiger <matthew.widiger@simplybusiness.com>
2020-01-31 12:56:31 -0500

    first pass file downloads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @report_topics = ['Energy']
   end
 end
---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,8 @@
+class ReportsController < ApplicationController
+  def index
+    @report_topic = params[:report_topic]
+    return unless @report_topic.present?
+    @files = Dir.entries("public/reports/#{@report_topic.downcase}")[2..-1]
+    binding.pry
+  end
+end
